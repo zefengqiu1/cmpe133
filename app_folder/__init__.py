68dbc2284ba6e16333727c7e7ba690fe1e836203
zefengqiu1 <zqiu6@go.pasadena.edu>
2020-12-15 12:07:17 -0800

    instruction

--- app_folder/__init__.py
+++ app_folder/__init__.py
@@ -18,9 +18,9 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = "zefengqiu1@gmail.com"
-app.config['MAIL_PASSWORD'] = "454576023"
-app.config['MAIL_DEFAULT_SENDER'] = "zefengqiu1@gmail.com"
+app.config['MAIL_USERNAME'] = "put your email address here"
+app.config['MAIL_PASSWORD'] = "*********"
+app.config['MAIL_DEFAULT_SENDER'] = "put your email address here"
 mail = Mail(app)
 
 from app_folder import routes
